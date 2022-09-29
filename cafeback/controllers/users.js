9221beb7a44353eeef71c3d9a1349e8dbfd50ded
Ezequiel Sanchez <ezegeek@gmail.com>
2022-09-29 17:28:06 -0300

    Se agregan las funciones de create, update, delete de User

--- cafeback/controllers/users.js
+++ cafeback/controllers/users.js
@@ -1,16 +1,75 @@
-const { validationResult } = require('express-validator')
-const bcrypt = require('bcryptjs')
-const { User } = require('../database/models')
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const { User } = require("../database/models");
 
-const controller = {
+const user = {
   list: async (req, res) => {
     try {
-    const users = await User.findAll({})
-    res.json(users)
+      const users = await User.findAll({});
+      res.json(users);
+    } catch (error) {
+      res.send(error);
     }
-    catch (error) { res.send(error);
-    }    
   },
-}
+  create: async (req, res) => {
+    const { firstName, lastName, email, password, roleId } = req.body;
 
-module.exports = controller
+    const createdUser = new User({
+      firstName,
+      lastName,
+      email,
+      password,
+      roleId,
+    });
+
+    try {
+      await createdUser.save();
+      return res.status(201).json({
+        user: createdUser,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        error: error,
+      });
+    }
+  },
+  update: async (req, res) => {
+    let userId = Number(req.params.id);
+
+    const { firstName, lastName, email, password, roleId } = req.body;
+
+    const updateData = {
+      firstName,
+      lastName,
+      email,
+      password,
+      roleId,
+    };
+
+    try {
+      let updatedUser = await User.update(updateData, {
+        where: {
+          id: userId,
+        },
+      });
+      res.send({ msg: "User actualizado correctamente" });
+    } catch (error) {
+      console.log("Error en el update: ", error);
+    }
+  },
+  delete: async (req, res) => {
+    let userId = Number(req.params.id);
+    try {
+      let softDeleteUser = await User.destroy({
+        where: {
+          id: userId,
+        },
+      });
+      res.send({ msg: `Se elimino el usuario con el id: ${userId}` });
+    } catch (error) {
+      console.log("Error en delete: ", error);
+    }
+  },
+};
+
+module.exports = user;
--- cafeback/routes/users.js
+++ cafeback/routes/users.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/users.js");
+const user = require("../controllers/users.js");
 
 const { body, checkSchema } = require("express-validator");
 
-router.get("/list", controller.list);
+router.get("/list", user.list);
+router.post("/create", user.create);
+router.patch("/update/:id", user.update);
+router.delete("/delete/:id", user.delete);
 
 module.exports = router;
