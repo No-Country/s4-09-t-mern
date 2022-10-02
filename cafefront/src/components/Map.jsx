b4a2edc3e0ed867dd6032a4c5913366c51260012
diegonicita <diegonicita@gmail.com>
2022-10-01 22:50:18 -0300

    Create SearchBar Component

--- cafefront/src/components/Map.jsx
+++ cafefront/src/components/Map.jsx
@@ -1,28 +1,41 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import React from 'react'
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import { useMapEvents } from 'react-leaflet/hooks'
+import React, { useState, useEffect } from 'react'
 import 'leaflet/dist/leaflet.css'
 import { MarkerIcon } from './MarkerIcon'
 import { cafeterias } from './cafeterias'
+import { SearchBar } from './SearchBar'
+
+const SetViewOnChange = (props) => {
+  const m = useMap()
+  m.setView(props.position, m.getZoom(), { animate: true })
+  return null
+}
 
 export const Map = () => {
+  const [position, setPosition] = useState([-34.6037, -58.3816])
+  const [zoom, setZoom] = useState(18)
+
   return (
-    <MapContainer
-      center={[-34.6037, -58.3816]}
-      zoom={18}
-      scrollWheelZoom={true}
-    >
-      <TileLayer
-        attribution=""
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {cafeterias.map((data) => (
-        <Marker key={data.id} icon={MarkerIcon} position={data.position}>
-          <Popup>
-            <strong>{data.name}</strong> <br /> {data.motto} <br />
-            <a href="http://www.google.com">Ver Cafeteria</a>
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+    <>
+      <MapContainer center={position} zoom={zoom} scrollWheelZoom={true}>
+        <TileLayer
+          attribution=""
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {cafeterias.map((data) => (
+          <Marker key={data.id} icon={MarkerIcon} position={data.position}>
+            <Popup>
+              <strong>{data.name}</strong> <br /> {data.motto} <br />
+              <a href="http://www.google.com">Ver Cafeteria</a>
+            </Popup>
+          </Marker>
+        ))}
+        <SetViewOnChange position={position} />
+      </MapContainer>
+      <SearchBar handlePosition={setPosition} />
+    </>
   )
 }
