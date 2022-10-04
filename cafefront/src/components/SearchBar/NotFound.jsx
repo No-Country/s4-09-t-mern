afd7fe5240d0b78dd9cf09f6d7fa74a00b46b123
diegonicita <diegonicita@gmail.com>
2022-10-04 00:35:28 -0300

    Change SearchBar styles and colors

--- cafefront/src/components/SearchBar/NotFound.jsx
+++ cafefront/src/components/SearchBar/NotFound.jsx
@@ -1,15 +1,7 @@
 import React from 'react'
 
 export const NotFound = () => (
-  <div
-    className="search-bar"
-    style={{
-      color: '#462E19',
-      backgroundColor: '#DED2BC',
-      padding: '0.5rem',
-      borderRadius: '10px'
-    }}
-  >
+  <div className="cafes-not-found">
     No hay cafes en la zona
   </div>
 )
