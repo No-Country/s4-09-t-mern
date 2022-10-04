e9d5de5cdca7999c01a0fb49255b6985b201e72a
Ezequiel Sanchez <ezegeek@gmail.com>
2022-10-04 16:12:07 -0300

    TI-012

--- cafeback/src/routes/user.routes.js
+++ cafeback/src/routes/user.routes.js
@@ -1,19 +1,8 @@
-const express = require('express')
-const router = express.Router()
-
-// const { getUserById, deleteUserById, updateUserById, addFavorite } = require('../controllers/user.controller')
-
-// const { checkJwt } = require('../config/auth0.config')
-// const addUserToAuth0 = require('../middleware/addUserToAuth0')
+const express = require('express');
+const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.status(200).send('user routes')
-})
+  res.status(200).send('user routes');
+});
 
-// router.post('/', checkJwt, addUserToAuth0, createUserController)
-// router.get('/:id', addUserToAuth0, getUserById)
-// router.put('/:id', addUserToAuth0, updateUserById)
-// router.delete('/:id', addUserToAuth0, deleteUserById)
-// router.post('/favorites/:id', addUserToAuth0, addFavorite)
-
-module.exports = router
+module.exports = router;
