3b1e7c1ecb800ea3430edd9640489a4303713078
diegonicita <diegonicita@gmail.com>
2022-10-06 20:13:26 -0300

    Merge Navbar. Fix styles

--- cafefront/src/pages/LandingPage/LandingPage.jsx
+++ cafefront/src/pages/LandingPage/LandingPage.jsx
@@ -4,9 +4,13 @@ import { Map } from '../../components/Map/Map'
 
 const LandingPage = () => {
   return (
-    <div className='container'>
-      <Header />
-      <Map />
+    <div className="container-fluid p-0">
+      <div className="row">
+        <div>
+          <Header />
+        </div>
+        <Map />
+      </div>
     </div>
   )
 }
