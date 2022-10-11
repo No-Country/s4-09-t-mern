c1cfe0499b4a0fc99a4ae60489707033fdc544ae
diegonicita <diegonicita@gmail.com>
2022-10-11 18:26:35 -0300

    Add redux to Project

--- cafefront/src/App.jsx
+++ cafefront/src/App.jsx
@@ -4,16 +4,20 @@ import LandingPage from './pages/LandingPage/LandingPage'
 import Login from './pages/Login/Login'
 import Ranking from './pages/Ranking/Ranking'
 import Register from './pages/Register/Register'
+import { Provider } from 'react-redux'
+import { store } from './redux/store/store'
 
 function App () {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<LandingPage />}></Route>
-        <Route path="ranking" element={<Ranking />}></Route>
-        <Route path="register" element={<Register />}></Route>
-        <Route path="login" element={<Login />}></Route>
-      </Routes>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<LandingPage />}></Route>
+          <Route path="ranking" element={<Ranking />}></Route>
+          <Route path="register" element={<Register />}></Route>
+          <Route path="login" element={<Login />}></Route>
+        </Routes>
+      </Provider>
     </>
   )
 }
--- cafefront/src/components/Header/NavbarDesktop.jsx
+++ cafefront/src/components/Header/NavbarDesktop.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import Icono from '../../../src/assets/images/cafeicon.jpg'
 import './Header.css'
+import { useMessageStore } from '../../redux/hooks/useMessage'
 
 const NavbarDesktop = () => {
+  // eslint-disable-next-line no-unused-vars
+  const { message, setMessage } = useMessageStore()
+
   return (
     <div className='row'>
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -21,7 +25,7 @@ const NavbarDesktop = () => {
           Login
         </NavLink> */}
         <NavLink to="/Register" className="nav-link">
-          Register
+          Register {message}
         </NavLink>
         </div>
       </div>
