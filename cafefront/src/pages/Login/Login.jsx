07826b0eae99638bb26bcea681c855b7b8b2c6dd
diegonicita <diegonicita@gmail.com>
2022-10-16 20:02:39 -0300

    Add formik and yup to Login and Register

--- cafefront/src/pages/Login/Login.jsx
+++ cafefront/src/pages/Login/Login.jsx
@@ -5,73 +5,117 @@ import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import axios from 'axios'
 import { useUserStore } from '../../redux/hooks/useUser'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
 
-export default function Login () {
-  const [dataForm, setDataForm] = useState({
+export default function Login() {
+  const initialValues = {
     email: '',
     password: ''
-  })
-  const [stateForm, setStateForm] = useState({ loading: false, error: false })
-  // eslint-disable-next-line no-unused-vars
+  }
+
+  const [stateForm, setStateForm] = useState({ loading: false, error: false }) 
   const { user, setUser } = useUserStore()
 
-  const handleChange = (e) => {
-    setDataForm((prevDataForm) => {
-      const newDataForm = {
-        ...prevDataForm
-      }
-      newDataForm[e.target.name] = e.target.value
-      return newDataForm
-    })
-  }
+  const validationSchema = yup.object().shape({
+    email: yup
+      .string()
+      .email('Debes ingresar un email valido')
+      .max(255)
+      .required('El campo no debe estar vacio'),
+    password: yup.string().required('El campo no debe estar vacio')
+  })
+  // .min(8, ({ min }) => `Tu password debe tener al menos ${min} caracteres`)
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
+  const onSubmit = (e) => {
     setStateForm({ loading: true, error: false })
-    axios.post('http://localhost:5000/api/v1/user/login', dataForm)
-      .then(res => {
-        setStateForm({ loading: false, error: false })
-        setUser(dataForm.email)
-        console.log(res)
+    const { email, password } = values
+    axios
+      .post('http://localhost:5000/api/v1/user/login', { email, password })
+      .then((res) => {
+        setStateForm({ loading: false, error: false })        
+        if (res.status === 200 && res.data.error !== true) setUser(email)
+        else {
+          setStateForm((p) => {
+            return { ...p, error: true }
+          })
+          setTimeout(() => {
+            setStateForm((p) => {
+              return { ...p, error: false }
+            })
+          }, 2000)
+        }
       })
-      .catch(error => {
+      .catch((error) => {
         setStateForm({ loading: false, error: true })
-        console.log(error)
+        setUser('')        
       })
   }
-  return (
-    <div className='row' style={{ margin: 0 }}>
-      <div>
-        <Header/>
-      </div>
-    <div className="container">
-    <section className="login">
-      <h1>Bienvenido</h1>
-      <h2>
-        Estas a la vuelta del mejor cafe
-        <img src={iconTazaCafe} alt="taza de cafe" className="icon-vapor" />
-      </h2>
-      <form className="form-login" onSubmit={handleSubmit}>
-        <label htmlFor="email">
-          Correo
-          <input type="text" name="email" value={dataForm.email} onChange={handleChange}/>
-        </label>
-        <label htmlFor="password">
-          Contraseña
-          <input type="password" name="password" value={dataForm.password} onChange={handleChange}/>
-        </label>
-        <button>{stateForm.loading ? 'Cargando...' : 'Iniciar Sesión' }</button>
-      </form>
 
-      <br />
-      <br />
-      <p>
-        ¿Aún no tienes una cuenta?<span> </span>
-        <a href="/register">Create una</a>
-      </p>
-    </section>
-    <Footer />
-    </div>
+  const formik = useFormik({ initialValues, validationSchema, onSubmit })
+  const { handleChange, handleSubmit, errors, touched, handleBlur, values } =
+    formik
+
+  return (
+    <div className="row" style={{ margin: 0 }}>
+      <div>
+        <Header />
+      </div>
+      <div className="container">
+        <section className="login">
+          <h1>Bienvenido</h1>
+          <h2>
+            Estas a la vuelta del mejor cafe
+            <img src={iconTazaCafe} alt="taza de cafe" className="icon-vapor" />
+          </h2>
+          <form className="form-login" onSubmit={handleSubmit}>
+            <label htmlFor="email">
+              Correo
+              <input
+                type="text"
+                name="email"
+                id="email"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                autoComplete="email"
+              />
+            </label>
+            {errors.email && touched.email && (
+              <div className="error">{errors.email}</div>
+            )}
+            <label htmlFor="password">
+              Contraseña
+              <input
+                type="password"
+                name="password"
+                id="password"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                autoComplete="current-password"
+              />
+            </label>
+            {errors.password && touched.password && (
+              <div className="error">{errors.password}</div>
+            )}
+            <button type="submit">
+              {stateForm.loading ? 'Cargando...' : 'Iniciar Sesión'}
+            </button>
+            {stateForm.error && (
+              <p className="error"> Credenciales invalidas</p>
+            )}
+          </form>
+
+          <br />
+          <br />
+          <p>
+            ¿Aún no tienes una cuenta?<span> </span>
+            <a href="/register">Create una</a>
+          </p>
+        </section>
+        <Footer />
+      </div>
     </div>
   )
 }
--- cafefront/src/pages/Register/Register.jsx
+++ cafefront/src/pages/Register/Register.jsx
@@ -5,90 +5,186 @@ import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import axios from 'axios'
 import { useUserStore } from '../../redux/hooks/useUser'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
 
-export default function Register () {
-  const [dataForm, setDataForm] = useState({
+export default function Register() {
+  const initialValues = {
     email: '',
     firstName: '',
     lastName: '',
     password: '',
     confirmPassword: ''
-  })
+  }
   const [stateForm, setStateForm] = useState({ loading: false, error: false })
   // eslint-disable-next-line no-unused-vars
   const { user, setUser } = useUserStore()
 
-  const handleChange = (e) => {
-    setDataForm((prevDataForm) => {
-      const newDataForm = {
-        ...prevDataForm
-      }
-      newDataForm[e.target.name] = e.target.value
-      console.log(newDataForm)
-      return newDataForm
-    })
-  }
+  const validationSchema = yup.object().shape({
+    email: yup
+      .string()
+      .email('Debes ingresar un email valido')
+      .max(255)
+      .required('El campo no debe estar vacio'),
+    firstName: yup
+      .string()
+      .min(
+        3,
+        ({ min }) => `Nombre muy corto. Debe tener al menos ${min} caracteres!`
+      )
+      .required('El campo no debe estar vacio'),
+    lastName: yup
+      .string()
+      .min(
+        3,
+        ({ min }) =>
+          `Apellido muy corto. Debe tener al menos ${min} caracteres!`
+      )
+      .required('El campo no debe estar vacio'),
+    password: yup
+      .string()
+      .min(8, ({ min }) => `Tu password debe tener al menos ${min} caracteres`)
+      .required('El campo no debe estar vacio'),
+    confirmPassword: yup
+      .string()
+      .min(8, ({ min }) => `Tu password debe tener al menos ${min} caracteres`)
+      .required('El campo no debe estar vacio')
+      .oneOf([yup.ref('password'), null], 'Tus passwords no coinciden')
+  })
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
+  const onSubmit = (e) => {
     setStateForm({ loading: true, error: false })
-    console.log(dataForm)
-    axios.post('http://localhost:5000/api/v1/user/register', dataForm)
-      .then(res => {
+    const { email, firstName, lastName, password, confirmPassword } = values
+    axios
+      .post('http://localhost:5000/api/v1/user/register', {
+        email,
+        firstName,
+        lastName,
+        password,
+        confirmPassword
+      })
+      .then((res) => {
         setStateForm({ loading: false, error: false })
         console.log(res)
-        setUser(dataForm.email)
+        if (res.status === 200 && res.data.error !== true) setUser(email)
+        else {
+          setStateForm((p) => {
+            return { ...p, error: true }
+          })
+          setTimeout(() => {
+            setStateForm((p) => {
+              return { ...p, error: false }
+            })
+          }, 2000)
+        }        
       })
-      .catch(error => {
+      .catch((error) => {
         setStateForm({ loading: false, error: true })
         console.log(error)
       })
   }
-  return (
-    <div className='row' style={{ margin: 0 }}>
-      <div>
-        <Header/>
-      </div>
-    <div className="container">
-    <section className="register">
-      <h1>Bienvenido</h1>
-      <h2>
-        Estas a la vuelta del mejor cafe
-        <img src={iconTazaCafe} alt="taza de cafe" className="icon-vapor" />
-      </h2>
-      <form className="form-register" onSubmit={handleSubmit}>
-        <label htmlFor="firstName">
-          Nombre
-          <input type="text" name="firstName" value={dataForm.firstName} onChange={handleChange}/>
-        </label>
-        <label htmlFor="lastName">
-          Apellido
-          <input type="text" name="lastName" value={dataForm.lastName} onChange={handleChange}/>
-        </label>
-        <label htmlFor="email">
-          Correo
-          <input type="text" name="email" value={dataForm.email} onChange={handleChange}/>
-        </label>
-        <label htmlFor="password">
-          Contraseña
-          <input type="password" name="password" value={dataForm.password} onChange={handleChange}/>
-        </label>
-        <label htmlFor="confirmPassword">
-          Confirma la contraseña
-          <input type="password" name="confirmPassword" value={dataForm.confirmPassword} onChange={handleChange}/>
-        </label>
-        <button>{stateForm.loading ? 'Cargando...' : 'Registrarme' }</button>
-      </form>
 
-      <br />
-      <br />
-      <p>
-        ¿Ya tienes una cuenta?<span> </span>
-        <a href="/login">Inicia sesion</a>
-      </p>
-    </section>
-    <Footer />
-    </div>
+  const formik = useFormik({ initialValues, validationSchema, onSubmit })
+  const { handleChange, handleSubmit, errors, touched, handleBlur, values } =
+    formik
+
+  return (
+    <div className="row" style={{ margin: 0 }}>
+      <div>
+        <Header />
+      </div>
+      <div className="container">
+        <section className="register">
+          <h1>Bienvenido</h1>
+          <h2>
+            Estas a la vuelta del mejor cafe
+            <img src={iconTazaCafe} alt="taza de cafe" className="icon-vapor" />
+          </h2>
+          <form className="form-register" onSubmit={handleSubmit}>
+            <label htmlFor="firstName">
+              Nombre
+              <input
+                type="text"
+                name="firstName"
+                id="firstName"
+                value={values.firstName}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </label>
+            {errors.firstName && touched.firstName && (
+              <div className='error'>{errors.firstName}</div>
+            )}
+            <label htmlFor="lastName">
+              Apellido
+              <input
+                type="text"
+                name="lastName"
+                id="lastName"
+                value={values.lastName}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </label>
+            {errors.lastName && touched.lastName && (
+              <div className='error'>{errors.lastName}</div>
+            )}
+            <label htmlFor="email">
+              Correo
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </label>
+            {errors.email && touched.email && <div className='error'>{errors.email}</div>}
+            <label htmlFor="password">
+              Contraseña
+              <input
+                type="password"
+                name="password"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </label>
+            {errors.password && touched.password && (
+              <div className='error'>{errors.password}</div>
+            )}
+            <label htmlFor="confirmPassword">
+              Confirma la contraseña
+              <input
+                type="password"
+                name="confirmPassword"
+                id="confirmPassword"
+                value={values.confirmPassword}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </label>
+            {errors.confirmPassword && touched.confirmPassword && (
+              <div className='error'>{errors.confirmPassword}</div>
+            )}
+            <button type="submit">
+              {stateForm.loading ? 'Cargando...' : 'Registrarme'}
+            </button>
+          {stateForm.error && (
+            <p className="error"> Error en la Registracion </p>
+            )}
+          </form>
+
+          <br />
+          <br />
+          <p>
+            ¿Ya tienes una cuenta?<span> </span>
+            <a href="/login">Inicia sesion</a>
+          </p>
+        </section>
+        <Footer />
+      </div>
     </div>
   )
 }
