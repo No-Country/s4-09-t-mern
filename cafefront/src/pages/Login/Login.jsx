183395c61824003d95871cad52e98283990311d2
kenny <kenny.luque.t@uni.pe>
2022-10-17 18:57:41 -0500

    feat: user validation, login error management

--- cafefront/src/pages/Login/Login.jsx
+++ cafefront/src/pages/Login/Login.jsx
@@ -6,14 +6,20 @@ import Footer from '../../components/Footer/Footer'
 import axios from 'axios'
 import { useUserStore } from '../../redux/hooks/useUser'
 
-export default function Login () {
+export default function Login() {
   const [dataForm, setDataForm] = useState({
     email: '',
     password: ''
   })
+
+  const [errorsLogin, setErrorsLogin] = useState("")
+
+
   const [stateForm, setStateForm] = useState({ loading: false, error: false })
   // eslint-disable-next-line no-unused-vars
   const { user, setUser } = useUserStore()
+
+  // const history = useHistory();
 
   const handleChange = (e) => {
     setDataForm((prevDataForm) => {
@@ -28,50 +34,65 @@ export default function Login () {
   const handleSubmit = (e) => {
     e.preventDefault()
     setStateForm({ loading: true, error: false })
-    axios.post('http://localhost:5000/api/v1/user/login', dataForm)
+    axios.post('http://localhost:5000/api/v1/user/login',
+      dataForm,
+      {
+        withCredentials: true
+      })
       .then(res => {
         setStateForm({ loading: false, error: false })
-        setUser(dataForm.email)
+        if (res.data.error) {
+          //Si existe error
+          setErrorsLogin(res.data.message);
+          console.log(res.data.message)
+        } else {
+          // history.push("/");
+          setUser(dataForm.email)
+        }
         console.log(res)
       })
       .catch(error => {
         setStateForm({ loading: false, error: true })
         console.log(error)
+
       })
   }
   return (
     <div className='row' style={{ margin: 0 }}>
       <div>
-        <Header/>
+        <Header />
       </div>
-    <div className="container">
-    <section className="login">
-      <h1>Bienvenido</h1>
-      <h2>
-        Estas a la vuelta del mejor cafe
-        <img src={iconTazaCafe} alt="taza de cafe" className="icon-vapor" />
-      </h2>
-      <form className="form-login" onSubmit={handleSubmit}>
-        <label htmlFor="email">
-          Correo
-          <input type="text" name="email" value={dataForm.email} onChange={handleChange}/>
-        </label>
-        <label htmlFor="password">
-          Contraseña
-          <input type="password" name="password" value={dataForm.password} onChange={handleChange}/>
-        </label>
-        <button>{stateForm.loading ? 'Cargando...' : 'Iniciar Sesión' }</button>
-      </form>
+      <div className="container">
+        <section className="login">
+          <h1>Bienvenido</h1>
+          <h2>
+            Estas a la vuelta del mejor cafe
+            <img src={iconTazaCafe} alt="taza de cafe" className="icon-vapor" />
+          </h2>
+          <form className="form-login" onSubmit={handleSubmit}>
+            <label htmlFor="email">
+              Correo
+              <input type="text" name="email" value={dataForm.email} onChange={handleChange} />
+            </label>
+            <label htmlFor="password">
+              Contraseña
+              <input type="password" name="password" value={dataForm.password} onChange={handleChange} />
+              <div>
+                {errorsLogin !== "" ? <span className="text-danger">{errorsLogin}</span> : null}
+              </div>
+            </label>
+            <button>{stateForm.loading ? 'Cargando...' : 'Iniciar Sesión'}</button>
+          </form>
 
-      <br />
-      <br />
-      <p>
-        ¿Aún no tienes una cuenta?<span> </span>
-        <a href="/register">Create una</a>
-      </p>
-    </section>
-    <Footer />
-    </div>
+          <br />
+          <br />
+          <p>
+            ¿Aún no tienes una cuenta?<span> </span>
+            <a href="/register">Create una</a>
+          </p>
+        </section>
+        <Footer />
+      </div>
     </div>
   )
 }
